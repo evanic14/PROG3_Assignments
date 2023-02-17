b46df59e9208d23b81f8dab8ab575876bd7b1562
emiel#0314 <44370997+evanic14@users.noreply.github.com>
2023-02-17 12:13:58 +0100

    assignment 3

--- han_elt_advancedc/LabExercisescode/Exercise-3.1/main.c
+++ han_elt_advancedc/LabExercisescode/Exercise-3.1/main.c
@@ -4,8 +4,29 @@
 
 bool isInt(const char str[]);
 
-int main(void)
+int main(int argc, char* argv[])
 {
+    int i;
+    int converted;
+    int total = 0;
+
+    printf("argc = %d\n\n", argc);
+
+    for (i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s\n", i, argv[i]);
+
+        if (isInt(argv[i]) == true)
+        {
+            converted = atoi(argv[i]);
+            total = total + converted;
+        }
+        else
+        {
+            // do nothing
+        }
+    }
+    printf("total = %d\n", total);
 
    return 0;
 }
